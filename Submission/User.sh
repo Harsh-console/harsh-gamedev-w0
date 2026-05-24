dbbbde32958f661869e88090c74862b364d2f04f
Harsh Singh <harsh.console004@gmail.com>
2026-05-25 00:10:22 +0530

    wrote User.sh

--- Submission/User.sh
+++ Submission/User.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #Participant to enter their details here:
 
-name="name goes here"
-Roll_no="roll number goes here"
-branch="branch goes here"
-mail="mail here @itbhu"
-phone="phone number here"
-remarks="remarks go here"
+name="Harsh Singh"
+Roll_no="25125023"
+branch="Mathematics and Computing"
+mail="harsh.singh.mat25@itbhu.ac.in"
+phone="8303723731"
+remarks=""
 #enter anything you want to add
 
 echo $name " | " $Roll_no " | " $branch " | " $remarks
